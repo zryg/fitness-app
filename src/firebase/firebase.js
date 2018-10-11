23076d4dd2bc4d7c4ccbf18ad4490c14de97d10b
Aaron Daberkow <aarondabs@gmail.com>
2018-10-11 14:59:27 -0500

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,130 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIRBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+
+
+
+
+
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+// });
+
+// const onValueChange = database.ref('expenses').on('value', (snapshot) => {
+//         const expenses = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+// database.ref('expenses')
+//     .once('value')
+//     .then((snapshot) => {
+//         const expenses = [];
+
+//         snapshot.forEach((childSnapshot) => {
+//             expenses.push({
+//                 id: childSnapshot.key,
+//                 ...childSnapshot.val()
+//             });
+//         });
+
+//         console.log(expenses);
+//     });
+
+// database.ref('expenses').push({
+//     description: 'rent',
+//     note: '',
+//     amount: 120000,
+//     createdAt: 1000
+// });
+
+
+
+
+// database.ref('notes/-LOYMu0ZXV9_WFj48iXj').remove();
+
+// database.ref('notes').push({
+//     title: 'Course Topics',
+//     body: 'react, angular, firebase'
+// });
+
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//     const data = snapshot.val();
+//     console.log(`${data.name} is a ${data.job.title} at ${data.job.company}`);
+// }, (e) => {
+//     console.log('Error with data fetching', e);
+// });
+
+//   database.ref('location/city')
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     })
+//     .catch((e) => {
+//         console.log('Error fetching data', e)
+//     });
+
+//   database.ref().set({
+//     name: 'Aaron Daberkow',
+//     age: 27,
+//     stressLevel: 6,
+//     job: {
+//         title: 'Project Engineer',
+//         company: 'Union Pacific'
+//     },
+//     location: {
+//         city: 'Omaha',
+//         country: 'United States'
+//     }
+//   }).then(() => {
+//       console.log('Data is saved!');
+//   }).catch((e) => {
+//     console.log('This failed.', e);
+//   });
+
+//   database.ref().update({
+//     stressLevel: 9,
+//     'job/company': 'Amazon',
+//     'location/city': 'Seattle'
+//   });
+
+// database.ref()
+//     .remove()
+//     .then(() => {
+//         console.log('Data was removed');
+//     })
+//     .catch((e) => {
+//         console.log('Did not remove data', e);
+//     });
