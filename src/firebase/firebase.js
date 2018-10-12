ec949c5dfef55e19b6b61ef02c7202c9b87a7ed8
Aaron Daberkow <aarondabs@gmail.com>
2018-10-12 16:06:06 -0500

    Updated google auth popup to always prompt for account after logoff

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,6 +13,9 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+googleAuthProvider.setCustomParameters({
+    prompt: 'select_account'
+});
 
 export { firebase, googleAuthProvider, database as default };
 
